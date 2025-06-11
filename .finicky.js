4f248dbc696090037b47341e838c7006a5c78c55
Raphaël Pinson <raphael@isovalent.com>
2025-06-11 12:57:57 +0200

    Update finicky config
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- .finicky.js
+++ .finicky.js
@@ -1,30 +1,34 @@
-module.exports = {
-    defaultBrowser: {
-       name: "Google Chrome",
-       profile: "Default",
+export default {
+  defaultBrowser: {
+    name: "Google Chrome",
+    profile: "Perso",
+  },
+  handlers: [
+    {
+      match: (url) => [
+        "www.geoguessr.com",
+        "midjourney.com",
+        "discord.com",
+        "youtube.com",
+        "youtu.be",
+        "x.com",
+        "bsky.app",
+        "www.linkedin.com",
+      ].includes(url.hostname),
+      browser: {
+        name: "Google Chrome",
+        profile: "Perso",
+      },
     },
-    handlers: [
-        {
-            match: finicky.matchHostnames([
-                "www.geoguessr.com", "midjourney.com", "discord.com", "youtube.com", "youtu.be",
-                "x.com", "bsky.app", "www.linkedin.com",
-            ]),
-            browser: {
-                name: "Google Chrome",
-                // Perso
-                profile: "Default",
-            },
-        },
-        {
-            match: ({ opener, urlString }) => {
-                finicky.log("Received URL", urlString, "from opener ID", opener.bundleId);
-                return opener.bundleId == "com.tinyspeck.slackmacgap";
-            },
-            browser: {
-                name: "Google Chrome",
-                // Work
-                profile: "Profile 1",
-            },
-        }
-    ],
-  };
+    {
+      match: (url, { opener }) => {
+        console.log("Received URL", url.href, "from opener:", opener?.bundleId);
+        return opener?.bundleId === "com.tinyspeck.slackmacgap";
+      },
+      browser: {
+        name: "Google Chrome",
+        profile: "Work",
+      },
+    },
+  ],
+};
